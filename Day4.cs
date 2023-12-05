5a3aefd220c91615339bed243184f19d16688b64
Eric Slutz <eric.slutz@icloud.com>
2023-12-05 06:24:19 -0500

    completed day 4 part 1

--- Day4.cs
+++ Day4.cs
@@ -1,3 +1,8 @@
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
 namespace Advent_of_Code_2023;
 
 public partial class Day4 : IDay
@@ -6,6 +11,7 @@ public partial class Day4 : IDay
     private readonly int _partOneAnswer;
     private readonly int _partTwoAnswer;
     private readonly List<Card> _cards;
+    // private int _total = 0;
 
     public Day4()
     {
@@ -18,7 +24,7 @@ public partial class Day4 : IDay
         }
 
         _partOneAnswer = Part1();
-        //_partTwoAnswer = Part2();
+        // _partTwoAnswer = Part2();
     }
 
     public void Run()
@@ -44,7 +50,29 @@ public partial class Day4 : IDay
 
     // private int Part2()
     // {
+    //     for (var x = 0; x < _cards.Count; x++)
+    //     {
+    //         _cards[x].Copies = GetCopies(_cards[x], x);
+    //     }
 
+    //     Console.WriteLine(JsonSerializer.Serialize(_cards));
+
+    //     return 0;
+    // }
+
+    // private List<Card> GetCopies(Card card, int currentIndex)
+    // {
+    //     var copies = new List<Card>();
+    //     var winningMatches = card.Numbers.Where(number => card.WinningNumbers.Contains(number)).Count();
+    //     for (var y = 1; y <= winningMatches; y++)
+    //     {
+    //         copies.Add(_cards[currentIndex + y]);
+
+    //         copies.Last().Copies = GetCopies(copies.Last(), copies.Count - 1);
+    //     }
+
+    //     _total += copies.Last().Copies.Count;
+    //     return copies;
     // }
 }
 
@@ -54,6 +82,7 @@ public class Card
     public int[] WinningNumbers { get; set; }
     public int[] Numbers { get; set; }
     public int Points { get; set; }
+    public List<Card> Copies { get; set; }
 
     private static readonly char[] separator = [':', '|'];
 
@@ -66,5 +95,6 @@ public class Card
         Numbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse).ToArray();
         Points = 0;
+        Copies = [];
     }
 }
